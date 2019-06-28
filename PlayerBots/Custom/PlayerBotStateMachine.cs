50b1c92d7d51a41e0e39689da9a98ec029402d86
Melledy <52122272+Melledy@users.noreply.github.com>
2019-06-27 22:14:33 -0700

    Improved spawning of playerbots

---
+++ PlayerBots/Custom/PlayerBotStateMachine.cs
@@ -0,0 +1,19 @@
+﻿using EntityStates;
+using EntityStates.AI.Walker;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerBots.Custom
+{
+    class PlayerBotStateMachine : EntityStateMachine
+    {
+        PlayerBotStateMachine()
+        {
+            this.customName = "AI";
+            this.initialStateType = new SerializableEntityStateType(typeof(Wander));
+            this.mainStateType = new SerializableEntityStateType(typeof(Wander));
+        }
+    }
+}
--- PlayerBots/PlayerBotManager.cs
+++ PlayerBots/PlayerBotManager.cs
@@ -1,8 +1,13 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
+using EntityStates;
+using EntityStates.AI.Walker;
 using PlayerBots.AI;
+using PlayerBots.Custom;
 using RoR2;
 using RoR2.CharacterAI;
+using RoR2.Navigation;
+using RoR2.Stats;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +20,7 @@ namespace PlayerBots
     public class PlayerBotManager : BaseUnityPlugin
     {
         public static System.Random random = new System.Random();
+
         public static List<GameObject> playerbots = new List<GameObject>();
         public static List<string> bodyList = new List<string> {
             "CommandoBody",
@@ -44,7 +50,7 @@ namespace PlayerBots
         private static ConfigWrapper<bool> AutoPurchaseItems { get; set; }
         private static ConfigWrapper<bool> HostOnlySpawnBots { get; set; }
         private static ConfigWrapper<bool> ShowNameplates { get; set; }
-        private static ConfigWrapper<bool> TreatBotsAsPlayers { get; set; }
+        private static ConfigWrapper<bool> SpawnAsPlayers { get; set; }
 
         public void Awake()
         {
@@ -65,34 +71,24 @@ namespace PlayerBots
 
             // Config
             InitialRandomBots = Config.Wrap("Initial Bots", "InitialRandomBots", "Starting amount of bots to spawn at the start of a run. (Random)", 0);
-            for (int i = 0; i < bodyProperNameList.Count; i++ )
+            for (int i = 0; i < bodyProperNameList.Count; i++)
             {
                 string name = bodyProperNameList[i];
                 InitialBots[i] = Config.Wrap("Initial Bots", "Initial" + name + "Bots", "Starting amount of bots to spawn at the start of a run. (" + name + ")", 0);
             }
-            
+
             AutoPurchaseItems = Config.Wrap("Bot Inventory", "AutoPurchaseItems", "Maximum amount of purchases a playerbot can do per stage. Items are purchased directly instead of from chests.", true);
             MaxBotPurchasesPerStage = Config.Wrap("Bot Inventory", "MaxBotPurchasesPerStage", "Maximum amount of putchases a playerbot can do per stage.", 8);
 
             HostOnlySpawnBots = Config.Wrap("Misc", "HostOnlySpawnBots", "Set true so that only the host may spawn bots", true);
-            ShowNameplates = Config.Wrap("Misc", "ShowNameplates", "Show player nameplates on playerbots. (Host only)", true);
+            ShowNameplates = Config.Wrap("Misc", "ShowNameplates", "Show player nameplates on playerbots if SpawnAsPlayers == false. (Host only)", true);
 
-            TreatBotsAsPlayers = Config.Wrap("Experimental", "TreatBotsAsPlayers", "Makes the game treat playerbots like how regular players are treated. The bots now show up on the scoreboard, can pick up items, influence the map scaling, etc.", false);
+            SpawnAsPlayers = Config.Wrap("Experimental", "SpawnAsPlayers", "Makes the game treat playerbots like how regular players are treated. The bots now show up on the scoreboard, can pick up items, influence the map scaling, etc.", false);
 
             // Hooks
             On.RoR2.Console.Awake += (orig, self) =>
             {
                 CommandHelper.RegisterCommands(self);
-                orig(self);
-            };
-
-            // Ugh.
-            On.RoR2.CharacterAI.BaseAI.OnBodyLost += (orig, self) =>
-            {
-                if (self.name.Equals("PlayerBot"))
-                {
-                    return;
-                }
                 orig(self);
             };
 
@@ -115,7 +111,7 @@ namespace PlayerBots
                 };
             }
 
-            if (!TreatBotsAsPlayers.Value && AutoPurchaseItems.Value)
+            if (!SpawnAsPlayers.Value && AutoPurchaseItems.Value)
             {
                 // Give bots money
                 On.RoR2.TeamManager.GiveTeamMoney += (orig, self, teamIndex, money) =>
@@ -167,13 +163,13 @@ namespace PlayerBots
                     orig(self);
                 };
             }
-            
-            On.RoR2.Stage.Start += (orig, self) => 
+
+            On.RoR2.Stage.Start += (orig, self) =>
             {
                 orig(self);
                 if (NetworkServer.active)
                 {
-                    if (TreatBotsAsPlayers.Value)
+                    if (SpawnAsPlayers.Value)
                     {
                         foreach (GameObject playerbot in playerbots.ToArray())
                         {
@@ -206,33 +202,21 @@ namespace PlayerBots
                     }
                 }
             };
-
-            if (TreatBotsAsPlayers.Value)
-            {
-                On.RoR2.RunReport.Generate += (orig, run, resultType) =>
-                {
-                    foreach (GameObject playerbot in playerbots.ToArray())
-                    {
-                        if (!playerbot)
-                        {
-                            playerbots.Remove(playerbot);
-                            continue;
-                        }
-
-                        PlayerCharacterMasterController masterController = playerbot.GetComponent<PlayerCharacterMasterController>();
-                        if (masterController)
-                        {
-                            DestroyImmediate(masterController);
-                        }
-                    }
-                    return orig(run, resultType);
-                };
-            }
- 
         }
 
-        // Also really hacky
         public static void SpawnPlayerbot(CharacterMaster owner, int characterType)
+        {
+            if (SpawnAsPlayers.Value)
+            {
+                SpawnPlayerbotAsPlayer(owner, characterType);
+            }
+            else
+            {
+                SpawnPlayerbotAsSummon(owner, characterType);
+            }
+        }
+
+        private static void SpawnPlayerbotAsPlayer(CharacterMaster owner, int characterType)
         {
             GameObject bodyPrefab = BodyCatalog.FindBodyPrefab(bodyList[characterType]);
             if (bodyPrefab == null)
@@ -240,7 +224,17 @@ namespace PlayerBots
                 return;
             }
 
-            SpawnCard card = (SpawnCard)Resources.Load("SpawnCards/CharacterSpawnCards/cscBeetleGuardAlly");
+            // Card
+            PlayerBotSpawnCard card = ScriptableObject.CreateInstance<PlayerBotSpawnCard>();
+            card.hullSize = HullClassification.Human;
+            card.nodeGraphType = MapNodeGroup.GraphType.Ground;
+            card.occupyPosition = false;
+            card.sendOverNetwork = true;
+            card.forbiddenFlags = NodeFlags.NoCharacterSpawn;
+            card.prefab = Resources.Load<GameObject>("prefabs/charactermasters/CommandoMaster");
+            card.playerbotName = bodyPrefab.GetComponent<CharacterBody>().GetDisplayName();
+
+            // Spawn
             DirectorSpawnRequest spawnRequest = new DirectorSpawnRequest(card, new DirectorPlacementRule
             {
                 placementMode = DirectorPlacementRule.PlacementMode.Approximate,
@@ -250,28 +244,26 @@ namespace PlayerBots
             }, RoR2Application.rng);
             spawnRequest.ignoreTeamMemberLimit = true;
             spawnRequest.summonerBodyObject = owner.GetBody().gameObject;
-            
+
             GameObject gameObject = DirectorCore.instance.TrySpawnObject(spawnRequest);
 
             if (gameObject)
             {
-                CharacterMaster master = gameObject.GetComponent<CharacterMaster>();
+                // Add components
+                EntityStateMachine stateMachine = gameObject.AddComponent<PlayerBotStateMachine>() as EntityStateMachine;
+                BaseAI ai = gameObject.AddComponent<PlayerBotBaseAI>() as BaseAI;
                 AIOwnership aiOwnership = gameObject.AddComponent<AIOwnership>() as AIOwnership;
-                BaseAI ai = gameObject.GetComponent<BaseAI>();
-                AISkillDriver[] skillDrivers = gameObject.GetComponents<AISkillDriver>();
+                aiOwnership.ownerMaster = owner;
 
-                PlayerCharacterMasterController playerMaster = null;
-                if (TreatBotsAsPlayers.Value)
-                {
-                    playerMaster = gameObject.AddComponent<PlayerCharacterMasterController>() as PlayerCharacterMasterController;
-                }
+                CharacterMaster master = gameObject.GetComponent<CharacterMaster>();
+                PlayerCharacterMasterController playerMaster = gameObject.GetComponent<PlayerCharacterMasterController>();
 
                 if (master)
                 {
+                    master.SetFieldValue("aiComponents", gameObject.GetComponents<BaseAI>());
+
                     master.bodyPrefab = bodyPrefab;
                     master.Respawn(master.GetBody().transform.position, master.GetBody().transform.rotation);
-
-                    master.teamIndex = TeamIndex.Player;
 
                     master.GiveMoney(owner.money);
                     master.inventory.CopyItemsFrom(owner.inventory);
@@ -280,11 +272,76 @@ namespace PlayerBots
 
                     // Allow the bots to spawn in the next stage
                     master.destroyOnBodyDeath = false;
-                    master.gameObject.AddComponent<SetDontDestroyOnLoad>();
+                    //master.gameObject.AddComponent<SetDontDestroyOnLoad>();
                 }
-                if (playerMaster && TreatBotsAsPlayers.Value)
+                if (playerMaster)
                 {
                     playerMaster.name = master.GetBody().GetDisplayName();
+                }
+
+                InjectSkillDrivers(gameObject, ai, characterType);
+
+                if (AutoPurchaseItems.Value)
+                {
+                    // Add item manager
+                    ItemManager itemManager = gameObject.AddComponent<ItemManager>() as ItemManager;
+                }
+
+                // Add to playerbot list
+                playerbots.Add(gameObject);
+
+                // Cleanup
+                Destroy(card);
+            }
+        }
+
+
+        // A hacky method. Don't ask questions.
+        private static void SpawnPlayerbotAsSummon(CharacterMaster owner, int characterType)
+        {
+            GameObject bodyPrefab = BodyCatalog.FindBodyPrefab(bodyList[characterType]);
+            if (bodyPrefab == null)
+            {
+                return;
+            }
+
+            // Get card
+            SpawnCard card = (SpawnCard)Resources.Load("SpawnCards/CharacterSpawnCards/cscBeetleGuardAlly");
+
+            // Spawn request
+            DirectorSpawnRequest spawnRequest = new DirectorSpawnRequest(card, new DirectorPlacementRule
+            {
+                placementMode = DirectorPlacementRule.PlacementMode.Approximate,
+                minDistance = 3f,
+                maxDistance = 40f,
+                spawnOnTarget = owner.GetBody().transform
+            }, RoR2Application.rng);
+            spawnRequest.ignoreTeamMemberLimit = true;
+            spawnRequest.summonerBodyObject = owner.GetBody().gameObject;
+
+            // Spawn
+            GameObject gameObject = DirectorCore.instance.TrySpawnObject(spawnRequest);
+
+            if (gameObject)
+            {
+                CharacterMaster master = gameObject.GetComponent<CharacterMaster>();
+                AIOwnership aiOwnership = gameObject.AddComponent<AIOwnership>() as AIOwnership;
+                BaseAI ai = gameObject.GetComponent<BaseAI>();
+
+                if (master)
+                {
+                    master.bodyPrefab = bodyPrefab;
+                    master.Respawn(master.GetBody().transform.position, master.GetBody().transform.rotation);
+
+                    //master.teamIndex = TeamIndex.Player;
+
+                    master.GiveMoney(owner.money);
+                    master.inventory.CopyItemsFrom(owner.inventory);
+
+                    master.inventory.GiveItem(ItemIndex.DrizzlePlayerHelper, 1);
+
+                    // Allow the bots to spawn in the next stage
+                    master.destroyOnBodyDeath = false;
                 }
                 if (aiOwnership)
                 {
@@ -300,41 +357,7 @@ namespace PlayerBots
                     ai.aimVectorMaxSpeed = 180f;
                 }
 
-                if (skillDrivers != null)
-                {
-                    // Strip skills
-                    StripSkills(skillDrivers);
-                }
-
-                // Add skill drivers based on class
-                switch (characterType)
-                {
-                    case 0:
-                        CommandoHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 1:
-                        ToolbotHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 2:
-                        HuntressHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 3:
-                        EngineerHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 4:
-                        ArtificerHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 5:
-                        MercenaryHelper.InjectSkills(gameObject, ai);
-                        break;
-                    case 6:
-                        REXHelper.InjectSkills(gameObject, ai);
-                        break;
-                }
-
-                // Set skill drivers
-                AISkillDriver[] skills = ai.GetFieldValue<AISkillDriver[]>("skillDrivers");
-                ai.SetFieldValue("skillDrivers", gameObject.GetComponents<AISkillDriver>());
+                InjectSkillDrivers(gameObject, ai, characterType);
 
                 if (AutoPurchaseItems.Value)
                 {
@@ -345,6 +368,46 @@ namespace PlayerBots
                 // Add to playerbot list
                 playerbots.Add(gameObject);
             }
+        }
+
+        private static void InjectSkillDrivers(GameObject gameObject, BaseAI ai, int characterType)
+        {
+            AISkillDriver[] skillDrivers = gameObject.GetComponents<AISkillDriver>();
+            if (skillDrivers != null)
+            {
+                // Strip skills
+                StripSkills(skillDrivers);
+            }
+
+            // Add skill drivers based on class
+            switch (characterType)
+            {
+                case 0:
+                    CommandoHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 1:
+                    ToolbotHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 2:
+                    HuntressHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 3:
+                    EngineerHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 4:
+                    ArtificerHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 5:
+                    MercenaryHelper.InjectSkills(gameObject, ai);
+                    break;
+                case 6:
+                    REXHelper.InjectSkills(gameObject, ai);
+                    break;
+            }
+
+            // Set skill drivers
+            AISkillDriver[] skills = ai.GetFieldValue<AISkillDriver[]>("skillDrivers");
+            ai.SetFieldValue("skillDrivers", gameObject.GetComponents<AISkillDriver>());
         }
 
         public static void SpawnPlayerbots(CharacterMaster owner, int characterType, int amount)
@@ -379,50 +442,6 @@ namespace PlayerBots
             {
                 DestroyImmediate(skill);
             }
-        }
-
-        private static void DumpAllSkills(GameObject gameObject)
-        {
-            Chat.AddMessage("All Skills:");
-            AISkillDriver[] component4 = gameObject.GetComponents<AISkillDriver>();
-            foreach (AISkillDriver skill in component4)
-            {
-                Chat.AddMessage("Name: " + skill.customName);
-                DumpSkill(skill);
-                Chat.AddMessage(" ");
-            }
-        }
-
-        public static void DumpSkill(AISkillDriver skill)
-        {
-            Chat.AddMessage(" ");
-            Chat.AddMessage("Name: " + skill.customName);
-            Chat.AddMessage("Slot: " + skill.skillSlot);
-            Chat.AddMessage("Ready: " + skill.requireSkillReady);
-
-            Chat.AddMessage("TargetMove: " + skill.moveTargetType);
-            Chat.AddMessage("MinUHp: " + skill.minUserHealthFraction);
-            Chat.AddMessage("MaxUHp: " + skill.maxUserHealthFraction);
-            Chat.AddMessage("MinTHP: " + skill.minTargetHealthFraction);
-            Chat.AddMessage("MaxTHp: " + skill.maxTargetHealthFraction);
-
-            Chat.AddMessage("MinDist: " + skill.minDistance);
-            Chat.AddMessage("MaxDist: " + skill.maxDistance);
-
-            Chat.AddMessage("SelectionTarget: " + skill.selectionRequiresTargetLoS);
-            Chat.AddMessage("AimTarget: " + skill.activationRequiresTargetLoS);
-            Chat.AddMessage("AimConfirm: " + skill.activationRequiresAimConfirmation);
-
-            Chat.AddMessage("MoveType: " + skill.movementType);
-            Chat.AddMessage("AimType: " + skill.aimType);
-            Chat.AddMessage("MoveScale: " + skill.moveInputScale);
-
-            Chat.AddMessage("DriverTimer: " + skill.driverUpdateTimerOverride);
-            Chat.AddMessage("IgnoreNode: " + skill.ignoreNodeGraph);
-            Chat.AddMessage("ResetEnemy: " + skill.resetCurrentEnemyOnNextDriverSelection);
-            Chat.AddMessage("NoRepeat: " + skill.noRepeat);
-            Chat.AddMessage("Sprint: " + skill.shouldSprint);
-            Chat.AddMessage(" ");
         }
 
         [ConCommand(commandName = "addbot", flags = ConVarFlags.ExecuteOnServer, helpText = "Adds a playerbot. Usage: addbot [character index] [amount] [network user index]")]
