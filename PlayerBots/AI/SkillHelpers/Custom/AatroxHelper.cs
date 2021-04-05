e117362c23fd19bc39ccf6127788a62854d5baa1
Melledy <52122272+Melledy@users.noreply.github.com>
2021-04-05 02:03:35 -0700

    Add back some custom characters that were removed while updating

---
+++ PlayerBots/AI/SkillHelpers/Custom/AatroxHelper.cs
@@ -0,0 +1,87 @@
+﻿using RoR2.CharacterAI;
+using UnityEngine;
+
+namespace PlayerBots.AI.SkillHelpers.Custom
+{
+    [SkillHelperSurvivor("AatroxBody")]
+    [CustomSurvivor("https://thunderstore.io/package/rob/Aatrox/", "4.1.0")]
+    class AatroxHelper : AiSkillHelper
+    {
+        public override void InjectSkills(GameObject gameObject, BaseAI ai)
+        {
+            AISkillDriver skill3 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            skill3.customName = "Utility";
+            skill3.skillSlot = RoR2.SkillSlot.Utility;
+            skill3.requireSkillReady = true;
+            skill3.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            skill3.minDistance = 0;
+            skill3.maxDistance = 80;
+            skill3.selectionRequiresTargetLoS = true;
+            skill3.activationRequiresTargetLoS = true;
+            skill3.activationRequiresAimConfirmation = true;
+            skill3.movementType = AISkillDriver.MovementType.Stop;
+            skill3.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            skill3.ignoreNodeGraph = false;
+            skill3.resetCurrentEnemyOnNextDriverSelection = false;
+            skill3.noRepeat = true;
+            skill3.shouldSprint = false;
+            skill3.buttonPressType = AISkillDriver.ButtonPressType.TapContinuous;
+
+            AISkillDriver chaseSkill = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            chaseSkill.customName = "ChaseTarget";
+            chaseSkill.skillSlot = RoR2.SkillSlot.None;
+            chaseSkill.requireSkillReady = false;
+            chaseSkill.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            chaseSkill.minDistance = 10;
+            chaseSkill.maxDistance = 60;
+            chaseSkill.selectionRequiresTargetLoS = true;
+            chaseSkill.activationRequiresTargetLoS = true;
+            chaseSkill.activationRequiresAimConfirmation = false;
+            chaseSkill.movementType = AISkillDriver.MovementType.ChaseMoveTarget;
+            chaseSkill.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            chaseSkill.ignoreNodeGraph = false;
+            chaseSkill.resetCurrentEnemyOnNextDriverSelection = false;
+            chaseSkill.noRepeat = false;
+            chaseSkill.shouldSprint = true;
+
+            AISkillDriver skill2 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            skill2.customName = "Secondary";
+            skill2.skillSlot = RoR2.SkillSlot.Secondary;
+            skill2.requireSkillReady = true;
+            skill2.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            skill2.minDistance = 0;
+            skill2.maxDistance = 10;
+            skill2.minUserHealthFraction = .75f;
+            skill2.selectionRequiresTargetLoS = true;
+            skill2.activationRequiresTargetLoS = true;
+            skill2.activationRequiresAimConfirmation = true;
+            skill2.movementType = AISkillDriver.MovementType.ChaseMoveTarget;
+            skill2.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            skill2.ignoreNodeGraph = true;
+            skill2.resetCurrentEnemyOnNextDriverSelection = false;
+            skill2.noRepeat = true;
+            skill2.shouldSprint = true;
+
+            // Skills
+            AISkillDriver skill1 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            skill1.customName = "Primary";
+            skill1.skillSlot = RoR2.SkillSlot.Primary;
+            skill1.requireSkillReady = true;
+            skill1.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            skill1.minDistance = 0;
+            skill1.maxDistance = 10;
+            skill1.selectionRequiresTargetLoS = true;
+            skill1.activationRequiresTargetLoS = true;
+            skill1.activationRequiresAimConfirmation = false;
+            skill1.movementType = AISkillDriver.MovementType.ChaseMoveTarget;
+            skill1.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            skill1.ignoreNodeGraph = true;
+            skill1.resetCurrentEnemyOnNextDriverSelection = false;
+            skill1.noRepeat = false;
+            skill1.shouldSprint = true;
+
+            // Add default skills
+            AddDefaultSkills(gameObject, ai, 0);
+        }
+    }
+}
